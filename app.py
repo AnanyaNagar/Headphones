5e545cc726cd06a8160385f083e2c2fa43d544ec
AnanyaNagar <60837177+AnanyaNagar@users.noreply.github.com>
2021-04-10 02:01:31 +0530

    Added functionality for head detection. Made the code a little clean. Added few images.

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask import Flask, render_template
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-
 # classifiers of various parts:
 eye_classifier = cv2.CascadeClassifier(f"{dir_path}"+r"\classifiers\eyes.xml")
 face_classifier = cv2.CascadeClassifier(f"{dir_path}"+r"\classifiers\haarcascade_frontalface_default.xml")
@@ -16,33 +15,79 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
-@app.route('/kids.html')
-def kds():
-    return render_template('kids.html')
 
-@app.route('/women.html')
-def wmn():
-    return render_template('womenj1.html')
+@app.route('/caps.html')
+def cap():
+    return render_template('caps.html')
 
-@app.route('/men.html')
-def mn():
-    return render_template('men.html')
 
-@app.route('/womenj1.html')
-def wdress():
+@app.route('/index.html')
+def index():
+    return render_template('index.html')
+
+# All available eyewears
+@app.route('/specs')
+def specs():
+    apply_goggles(specs.__name__)
+    return render_template('index.html')
+
+@app.route('/glass1')
+def glass1():
+    apply_goggles(glass1.__name__)
+    return render_template('index.html')
+
+@app.route('/blueGlass')
+def blueGlass():
+    apply_goggles(blueGlass.__name__)
+    return render_template('index.html')
+
+@app.route('/orangeGlass')
+def orangeGlass():
+    apply_goggles(orangeGlass.__name__)
+    return render_template('index.html')
+
+
+# All available head wears
+@app.route('/cap1')
+def cap1():
+    apply_cap(cap1.__name__)
+    return render_template('caps.html')
+
+@app.route('/cap2')
+def cap2():
+    apply_cap(cap2.__name__)
+    return render_template('caps.html')
+
+@app.route('/crown')
+def crown():
+    apply_cap(crown.__name__)
+    return render_template('caps.html')
+
+@app.route('/headbow')
+def headbow():
+    apply_cap(headbow.__name__)
+    return render_template('caps.html')
+
+
+def image_resize(img, req_width):
+    r = float(req_width) / img.shape[1]
+    dim = (req_width, int(img.shape[0] * r))
+    img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+    return img
+
+
+def apply_goggles(name):
     cap = cv2.VideoCapture(0)
-    def image_resize(img, req_width):
-        r = float(req_width) / img.shape[1]
-        dim = (req_width, int(img.shape[0] * r))
-        img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
-        return img
+
 
     while cap.isOpened():
         ret, frame = cap.read()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
 
-        glasses = cv2.imread(f"{dir_path}" + r"\images\specs.png", -1)
+        print(f"{dir_path}" + fr"\static\{name}.png")
+        glasses = cv2.imread(f"{dir_path}" + fr"\static\{name}.png", -1)
+        print(glasses)
 
         if ret:
             faces = face_classifier.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
@@ -79,47 +124,32 @@ def wdress():
 
     cap.release()
     cv2.destroyAllWindows()
-    return "Head back to the previous page"
 
 
-@app.route('/men1.html')
-def mdress():
+def apply_cap(name):
     cap = cv2.VideoCapture(0)
-    def image_resize(img, req_width):
-        r = float(req_width) / img.shape[1]
-        dim = (req_width, int(img.shape[0] * r))
-        img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
-        return img
 
     while cap.isOpened():
         ret, frame = cap.read()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
 
-        glasses = cv2.imread(f"{dir_path}" + r"\images\specs.png", -1)
+        print(f"{dir_path}" + fr"\images\{name}.png")
+        cap_img = cv2.imread(f"{dir_path}" + fr"\static\{name}.png", -1)
+        print(cap_img)
 
         if ret:
             faces = face_classifier.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
             for face in faces:
                 x, y, w, h = face
-                roi_gray = gray[y:y + h, x:x + w]
-                roi_color = frame[y:y + h, x:x + w]
                 # cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 255), 3)
-                # eye classifier:
-                eyes = eye_classifier.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
-                # plotting eyes:
-                for eye in eyes:
-                    ex, ey, ew, eh = eye
-                    region_of_eyes = roi_gray[ey:ey + eh, ex:ex + ew]
-                    # cv2.rectangle(frame, (ex,ey), (ex+ew,ey+eh), (0,255,0), 3)
-                    glasses = image_resize(glasses, ew)
-
-                    gh, gw, gc = glasses.shape
-                    # print(glasses.shape, eye, frame.shape)
-                    for i in range(gh):
-                        for j in range(gw):
-                            if glasses[i, j][3] != 0:
-                                frame[ey + i + 20, ex + j] = glasses[i, j]
+                cap_img = image_resize(cap_img, w)
+                #
+                gh, gw, gc = cap_img.shape
+                for i in range(gh):
+                    for j in range(gw):
+                        if cap_img[i, j][3] != 0:
+                            frame[y + i - (h // 2), x + j] = cap_img[i, j]
 
             frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)
         cv2.imshow("dressing room(q-quit, c-click)", frame)
@@ -133,61 +163,6 @@ def mdress():
 
     cap.release()
     cv2.destroyAllWindows()
-    return "Head back to the previous page"
-
-
-@app.route('/kids1.html')
-def kdress():
-    cap = cv2.VideoCapture(0)
-    def image_resize(img, req_width):
-        r = float(req_width) / img.shape[1]
-        dim = (req_width, int(img.shape[0] * r))
-        img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
-        return img
-
-    while cap.isOpened():
-        ret, frame = cap.read()
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
-
-        glasses = cv2.imread(f"{dir_path}" + r"\images\specs.png", -1)
-
-        if ret:
-            faces = face_classifier.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
-            for face in faces:
-                x, y, w, h = face
-                roi_gray = gray[y:y + h, x:x + w]
-                roi_color = frame[y:y + h, x:x + w]
-                # cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 255), 3)
-                # eye classifier:
-                eyes = eye_classifier.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
-                # plotting eyes:
-                for eye in eyes:
-                    ex, ey, ew, eh = eye
-                    region_of_eyes = roi_gray[ey:ey + eh, ex:ex + ew]
-                    # cv2.rectangle(frame, (ex,ey), (ex+ew,ey+eh), (0,255,0), 3)
-                    glasses = image_resize(glasses, ew)
-
-                    gh, gw, gc = glasses.shape
-                    # print(glasses.shape, eye, frame.shape)
-                    for i in range(gh):
-                        for j in range(gw):
-                            if glasses[i, j][3] != 0:
-                                frame[ey + i + 20, ex + j] = glasses[i, j]
-
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)
-        cv2.imshow("dressing room(q-quit, c-click)", frame)
-
-        # If 'q' key from he keyboard is pressed, save the image in the system
-        key = cv2.waitKey(10)
-        if key == ord("q"):
-            break
-        if key == ord("c"):
-            cv2.imwrite("MyImage.jpg", frame)
-
-    cap.release()
-    cv2.destroyAllWindows()
-    return "Head back to the previous page"
 
 
 if __name__ == "__main__":
--- dressing_room.py
+++ dressing_room.py
@@ -1,3 +1,14 @@
+import cv2
+import os
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+# classifiers of various parts:
+eye_classifier = cv2.CascadeClassifier(f"{dir_path}"+r"\classifiers\eyes.xml")
+face_classifier = cv2.CascadeClassifier(f"{dir_path}"+r"\classifiers\haarcascade_frontalface_default.xml")
+upperbody_classifier = cv2.CascadeClassifier(f"{dir_path}"+r"\classifiers\haarcascade_upperbody.xml")
+
+cap = cv2.VideoCapture(0)
 
 def image_resize(img, req_width):
     r = float(req_width) / img.shape[1]
@@ -9,31 +20,20 @@ while cap.isOpened():
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
-
-    glasses = cv2.imread(f"{dir_path}"+r"\images\specs.png", -1)
+    cap_img = cv2.imread(f"{dir_path}"+r"\images\cap1.png", -1)
 
     if ret:
         faces = face_classifier.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
         for face in faces:
             x, y, w, h = face
-            roi_gray = gray[y:y + h, x:x + w]
-            roi_color = frame[y:y + h, x:x + w]
             # cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 255), 3)
-            # eye classifier:
-            eyes = eye_classifier.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
-            # plotting eyes:
-            for eye in eyes:
-                ex, ey, ew, eh = eye
-                region_of_eyes = roi_gray[ey:ey + eh, ex:ex + ew]
-                # cv2.rectangle(frame, (ex,ey), (ex+ew,ey+eh), (0,255,0), 3)
-                glasses = image_resize(glasses, ew)
-
-                gh, gw, gc = glasses.shape
-                # print(glasses.shape, eye, frame.shape)
-                for i in range(gh):
-                    for j in range(gw):
-                        if glasses[i, j][3] != 0:
-                            frame[ey + i + 20, ex + j] = glasses[i, j]
+            cap_img = image_resize(cap_img, w)
+            #
+            gh, gw, gc = cap_img.shape
+            for i in range(gh):
+                for j in range(gw):
+                    if cap_img[i, j][3] != 0:
+                        frame[y + i - (h//2), x + j] = cap_img[i, j]
 
         frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)
     cv2.imshow("dressing room(q-quit, c-click)", frame)
